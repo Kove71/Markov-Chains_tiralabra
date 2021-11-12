e02517c3244b1adf791e98482e65b5ef269ad84f
Kosti Vesterinen <kosti.vesterinen@helsinki.fi>
2021-11-12 21:30:58 +0200

    testaus corpus_data.py

---
+++ src/tests/corpus_data_test.py
@@ -0,0 +1,10 @@
+import unittest
+import corpus_data
+
+class TestCorpusdata(unittest.TestCase):
+
+    def setUp(self):
+        self.text = corpus_data.get_corpus("test.txt")
+    
+    def test_get_corpus(self):
+        self.assertAlmostEqual("testi", self.text)
