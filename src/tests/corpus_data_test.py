52291561842c3128bf73db31f91bfeb98a20d276
Kosti Vesterinen <kosti.vesterinen@helsinki.fi>
2021-11-13 22:11:59 +0200

    dokumentointia

--- src/tests/corpus_data_test.py
+++ src/tests/corpus_data_test.py
@@ -5,6 +5,6 @@ class TestCorpusdata(unittest.TestCase):
 
     def setUp(self):
         self.text = corpus_data.get_corpus("test.txt")
-    
+
     def test_get_corpus(self):
         self.assertAlmostEqual("testi", self.text)
--- src/ui/app_widget.py
+++ src/ui/app_widget.py
@@ -19,13 +19,26 @@ from markov_chain import generate_sentence
 
 class ApplicationWidget(QWidget):
 
+    """Käyttöliittymästä vastaava luokka. Perii PyQt5:n QWidget luokan
+    ja ui:n pääikkunan.
+    """
+
     def __init__(self, parent):
+
+        """Luokan konstruktori.
+        Args:
+            parent: pääikkuna, johon luokka "kiinnittyy"
+        """
 
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
         self.setup_ui()
 
     def setup_ui(self):
+
+        """Alustaa käyttöliittymäkomponentit ja niiden asettelun.
+        """
+
         self.generate_button = QPushButton("Generate!")
         self.generate_button.clicked.connect(self.generate_button_clicked)
         self.text_box = QTextEdit()
@@ -36,5 +49,11 @@ class ApplicationWidget(QWidget):
         self.setLayout(self.layout)
 
     def generate_button_clicked(self):
+
+        """Suoritetaan, kun painetaan "Generate!"-nappia. 
+        Kutsuu generate_sentence-metodia ja tulostaa sen tuottaman
+        lauseen käyttöliittymään.
+        """
+
         text = generate_sentence(1, "greatgatsby.txt")
         self.text_box.setText(text)
